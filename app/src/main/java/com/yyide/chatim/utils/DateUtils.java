6a9475aec6a960aa5abe0fb1ceec432e25ac9a20
program008 <1937553940@qq.com>
2021-07-02 15:52:38 +0800

    修改考勤统计 节次修改

--- app/src/main/java/com/yyide/chatim/utils/DateUtils.java
+++ app/src/main/java/com/yyide/chatim/utils/DateUtils.java
@@ -392,7 +392,7 @@ public class DateUtils {
         if (section == 0){
             return "早读";
         }
-        return String.format(context.getString(R.string.attendance_class_section),sectionUppercase(section));
+        return String.format(context.getString(R.string.attendance_class_section),convert(section));
     }
 
     public static String sectionUppercase(int section) {
@@ -421,6 +421,58 @@ public class DateUtils {
                 break;
         }
         return "" + section;
+    }
+
+    public static String convert(int number) {
+        String[] single = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"};//汉字一到九
+        String[] unit = {"", "十", "百", "千", "万", "亿"};//汉字单位
+        StringBuilder sb = new StringBuilder();
+        int[] unitInt = {100000000, 10000, 1000, 100, 10};
+        int m = 0;
+        int tmp = -1;
+        for (int i : unitInt) {
+            int n = number / i;
+            number = number % i;
+            if (sb.length() != 0 || n != 0) {
+                if (n > single.length - 1) {
+                    String cc = convert(n);
+                    sb.append(cc);
+                } else {
+                    if (0==tmp && 0==n) {
+
+                    } else {
+                        sb.append(single[n]);
+                    }
+
+                    tmp = n;
+                }
+                if (n != 0) {
+                    sb.append(unit[unit.length - 1 - m]);
+                }
+
+            }
+            m++;
+
+        }
+        if (number != 0) {
+            sb.append(single[number]);
+        }
+        String ret = sb.toString();
+        if (ret.length()==0){
+            return "零";
+        }
+        String last = String.valueOf(ret.charAt(ret.length() - 1));
+        if ("零".equals(last)) {
+            ret = ret.substring(0, ret.length() - 1);
+        }
+
+        if (sb.length() >=2){
+            if (sb.charAt(0)=='一' && sb.charAt(1) == '十'){
+                sb.deleteCharAt(0);
+                ret = sb.toString();
+            }
+        }
+        return ret;
     }
 
     public static boolean minMonth(String beginData, int currentMonth) {
