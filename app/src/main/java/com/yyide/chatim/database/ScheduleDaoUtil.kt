d99a9c276538ac224d0d464413a32f6f250f1546
program008 <1937553940@qq.com>
2021-11-09 11:36:58 +0800

    【11.6日创建的会议，时间是11.6-11.8日，11.8日进入日程--今日清单页面，没有显示“11.6日-11.8日会议”的日程】

--- app/src/main/java/com/yyide/chatim/database/ScheduleDaoUtil.kt
+++ app/src/main/java/com/yyide/chatim/database/ScheduleDaoUtil.kt
@@ -146,6 +146,18 @@ object ScheduleDaoUtil {
                     newSchedule.startTime = dataTime.toString("yyyy-MM-dd HH:mm:ss")
                     newSchedule.endTime = dataTime2.toString("yyyy-MM-dd HH:mm:ss")
                     listAllSchedule.add(newSchedule)
+                } else if (it.simplifiedDataTime() < DateTime.now().simplifiedDataTime()) {
+                    //计算不是今日的日程(今日之前)，跨天是否在今日显示
+                    logd("---计算不是今日的日程(今日之前)，跨天是否在今日显示--")
+                    loge(
+                        "dataTime=${it}, schedule=[name=${schedule.name}, date=${schedule.startTime} <-> ${schedule.endTime}]"
+                    )
+                    val startTime = toDateTime(schedule.startTime)
+                    val endTime = toDateTime(schedule.endTime)
+                    val now = DateTime.now().simplifiedDataTime()
+                    if (now in startTime..endTime) {
+                        listAllSchedule.add(schedule)
+                    }
                 }
             }
         }
