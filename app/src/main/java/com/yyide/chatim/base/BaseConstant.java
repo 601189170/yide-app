399fccaf6dba942cf052c0a01cf24c98df1230c1
lrz <lrz@yyide.com>
2022-01-11 13:39:58 +0800

    修改点击消息待办闪退问题

--- app/src/main/java/com/yyide/chatim/base/BaseConstant.java
+++ app/src/main/java/com/yyide/chatim/base/BaseConstant.java
@@ -47,7 +47,7 @@ public class BaseConstant {
 //    public static final String API_SERVER_URL_UAT = "http://192.168.3.108:8888";//Dev 环境
 
     public static final String API_SERVER_URL_RELEASE = "https://api.edu.1d1j.cn";
-    public static final String API_SERVER_URL_UAT = "https://api.uat.edu.1d1j.net";
+    public static final String API_SERVER_URL_UAT = "https://api.uat.edu.1d1j.cn";
 
     //Http访问地址
     public static String API_SERVER_URL = BuildConfig.DEBUG ? API_SERVER_URL_UAT : API_SERVER_URL_RELEASE;
@@ -70,7 +70,7 @@ public class BaseConstant {
     //通知公告模板地址
 //    public static final String TEMPLATE_URL_DEV = "http://192.168.3.147:8005/#/notice/edit/";
     public static final String TEMPLATE_URL_RELEASE = "https://wap.edu.1d1j.cn/#/notice/edit/";
-    public static final String TEMPLATE_URL_DEV = "https://app.uat.edu.1d1j.net/#/notice/edit/";
+    public static final String TEMPLATE_URL_DEV = "https://app.uat.edu.1d1j.cn/#/notice/edit/";
 
     public static final String TEMPLATE_URL = BuildConfig.DEBUG ? TEMPLATE_URL_DEV : TEMPLATE_URL_RELEASE;
 
---
+++ app/src/main/java/com/yyide/chatim/model/WebParamsUser.kt
@@ -0,0 +1,3 @@
+package com.yyide.chatim.model
+
+data class WebParamsUser(val userId: String?, val schoolId: Long?)
