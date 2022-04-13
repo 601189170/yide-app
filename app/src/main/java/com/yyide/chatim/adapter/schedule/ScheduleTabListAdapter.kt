e5b5d362bb7296329ca364c299eaef31175935c0
jzy <946809018@qq.com>
2022-04-13 10:15:51 +0800

    修改日程部分bug

---
+++ app/src/main/java/com/yyide/chatim/adapter/schedule/ScheduleTabListAdapter.kt
@@ -0,0 +1,9 @@
+package com.yyide.chatim.adapter.schedule
+
+/**     
+  * @ProjectName : yideapp
+  * @Author : J
+  * @Time : 2022/4/12 14:34
+  * @Description : 文件描述 
+ */class ScheduleTabListAdapter {
+}
